681e7ee6a53d4f905991f744eb89ad79ab434245
Igor Sthanny <igorsthaynny@gmail.com>
2025-03-25 21:48:28 -0300

    feat: injetando as dependencias

--- lib/core/dependecy/dependency.dart
+++ lib/core/dependecy/dependency.dart
@@ -1,5 +1,17 @@
 import 'package:get_it/get_it.dart';
+import 'package:marvels_api/features/home/view/home_view_model.dart';
+import 'package:marvels_api/features/shared/characters/data/repositories/character_repository_imp.dart';
+import 'package:marvels_api/features/shared/characters/data/services/character_service.dart';
+import 'package:marvels_api/features/shared/characters/domain/repositories/character_repository.dart';
 
 final dependency = GetIt.instance;
 
-void setupDependency() {}
+void setupDependency() {
+  dependency.registerFactory(CharacterService.new);
+  dependency.registerFactory<CharacterRepository>(
+    () => CharacterRepositoryImp(characterService: dependency()),
+  );
+  dependency.registerFactory(
+    () => HomeViewModel(characterRepository: dependency()),
+  );
+}
