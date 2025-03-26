772aa5d5af302ffafc1910c09d28ce7b0ce46ea4
Igor Sthanny <igorsthaynny@gmail.com>
2025-03-26 00:56:36 -0300

    test: criando teste de componentes pagina de detalhes

--- integration_test/components/details_screen_components_test.dart
+++ integration_test/components/details_screen_components_test.dart
@@ -1,14 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:marvels_api/core/utils/string/strings.dart';
 import 'package:marvels_api/features/details/details_character_screen.dart';
-import 'package:marvels_api/features/home/view/widgets/card_character_widget.dart';
-import 'package:marvels_api/features/home/view/widgets/carrousel_characters_widget.dart';
 import 'package:marvels_api/features/home/view/widgets/image_widget.dart';
-import 'package:marvels_api/features/shared/characters/domain/repositories/character_repository.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:patrol/patrol.dart';
 
 import '../../test/feature/mock/character_mock.dart';
-
 
 void main() {
   group('HomeScreen', () {
@@ -19,9 +16,30 @@ void main() {
 
       await $.pumpAndSettle();
 
-      expect(find.byType(CardCharacterWidget), findsWidgets);
-      expect(find.byType(CarrouselCharactersWidget), findsOneWidget);
-      expect(find.byType(ImageWidget), findsWidgets);
+      expect(
+        find.byWidgetPredicate(
+          (widget) => widget is Text && widget.data == detalhesPersonagemString,
+        ),
+        findsOneWidget,
+      );
+      expect(
+        find.byWidgetPredicate(
+          (widget) =>
+              widget is Text && widget.data == tInstanceCharacterModel.name,
+        ),
+        findsOneWidget,
+      );
+      expect(
+        find.byWidgetPredicate(
+          (widget) =>
+              widget is Text &&
+              widget.data == tInstanceCharacterModel.description,
+        ),
+        findsOneWidget,
+      );
+      expect(find.byType(ImageWidget), findsOneWidget);
     });
   });
 }
+
+void validationsDetailsCharacterScreen(PatrolIntegrationTester $) {}
